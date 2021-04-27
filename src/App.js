e004f7548222ead216b5c863508e2a448fbe435d
abusaurisufian <asufian917@gmail.com>
2021-04-28 02:35:16 +0600

    V0.0.1

---
+++ src/App.js
@@ -0,0 +1,83 @@
+
+import React, { Component } from 'react'
+import Logo from './assets/bg.JPG';
+import classes from './App.module.css'
+import Card from './Card';
+import {studentList} from './studentList';
+import {topicList} from './topicList';
+class  App extends Component{
+  state={
+    students:studentList,
+    student:"",
+    topicList:topicList,
+    topic:"",
+    prevList:[],
+    prevTopic:0
+  }
+  getRandom=(len)=>{
+    let index=Math.floor(Math.random() * len);
+    return index;
+  }
+  showStudent=()=>{
+    let i=this.state.students.length;
+    let index=this.getRandom(i); 
+    let prev=this.state.prevList;
+    while(prev.find((e)=> e=== index)){ 
+      index=Math.floor(Math.random() * i);
+    }
+    this.setState({student:this.state.students[index]})
+    prev.push(index);
+    this.setState({prevList:prev})
+   
+  }
+  showTopic=()=>{
+    let i=this.state.topicList.length;
+    let index=this.getRandom(i); 
+    while(this.state.prevTopic === index){ 
+      index=Math.floor(Math.random() * i);
+    }
+    
+    this.setState({topic:this.state.topicList[index]})
+    this.setState({prevTopic:index})
+  }
+  render(){ 
+    let student=null;
+    let topic=null;
+    if(this.state.student.length === 0){
+      student=<Card data="Click The Button Below to Start"/>
+    }
+    else{
+      student= <Card data={this.state.student}/>;
+    }
+    if(this.state.topic.length === 0){
+      topic=<Card data="Click The Button Below to Start"/>
+    }
+    else{
+      topic= <Card data={this.state.topic}/>;
+    }
+  return (
+    <div>
+    <div className={classes.Bg}>
+      <div className={classes.CardContainer}>
+     {student}
+      <div>
+        <button onClick={this.showStudent} >Generate Student Name</button>
+      </div>
+      </div>
+      <div className={classes.CardContainer}>
+     {topic}
+      <div>
+        <button onClick={this.showTopic}>Generate Topic </button>
+      </div>
+      </div>
+    <span style={{color:'white', position:'absolute',bottom:'50px', right:'10px'}} >
+      copyright &#169; AREA69
+    </span>
+    </div>
+    </div>
+
+  );
+  }
+}
+
+export default App;
---
+++ src/topicList.js
@@ -0,0 +1,12 @@
+export let topicList=[
+    "The zombie apocalypse is coming, who are 3 people you want on your team?",
+    "How do you assess yourself?",
+    "most happiest day or moments of their life ",
+    "The biggest fear",
+    "Do you think you can stay committed to IHRS in the long run? What if you are offered a good position at other clubs? ",
+    "Where do you want to see yourself after 10 years?",
+    "If you had to name a clever thing that you have done, what would it be?",
+    "Describe yourself in 3 words.",
+    "10 things in common" 
+    
+    ]
