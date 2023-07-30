29f73a07305c345a930e4e0671ff49abbb4d359f
sufi-an <asufian917@gmail.com>
2023-07-31 01:32:15 +0600

    cicdtest

--- src/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './Card.module.css';
-const card=(props)=>(
-    <div className={classes.Card}>
-        {props.data}
-    </div>
-)
-
-export default card;
